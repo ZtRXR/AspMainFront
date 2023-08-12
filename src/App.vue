a3889b830bfa8a40267a0e3f66c2094b51dfc946
ZtRXR <Zengtudor@outlook.com>
2023-08-13 00:21:43 +0800

    基本的主页布局

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <script setup>
 
+import ASide from "@/views/Apps/ASide.vue";
 </script>
 <template>
-
+  <div class="common-layout">
+    <el-container>
+      <el-header style="height: 40px;display: flex;justify-content: center">
+        <el-text type="success" style="font-size: large">
+          Zengtudor Web
+        </el-text>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <ASide></ASide>
+        </el-aside>
+        <el-container>
+          <el-main><RouterView/></el-main>
+          <el-footer>
+            <el-text type="info" style="display: flex;justify-content: center">作者:曾子愚 技术:Vue3,Vite,route,ElementUI...</el-text>
+          </el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
+  </div>
 </template>
+
 
 <style scoped>
 
